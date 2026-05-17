52456a89c1af142feb73f130073af57db788c792
nin049 <122114506+nin049@users.noreply.github.com>
2026-05-17 20:39:06 +0200

    Track Xcode project files directly

---
+++ GluGlu/GluGlu/GluGluApp.swift
@@ -0,0 +1,32 @@
+//
+//  GluGluApp.swift
+//  GluGlu
+//
+//  Created by Nino Guinberteau on 17/05/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GluGluApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GluGlu/GluGluTests/GluGluTests.swift
@@ -0,0 +1,19 @@
+//
+//  GluGluTests.swift
+//  GluGluTests
+//
+//  Created by Nino Guinberteau on 17/05/2026.
+//
+
+import Testing
+@testable import GluGlu
+
+struct GluGluTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
